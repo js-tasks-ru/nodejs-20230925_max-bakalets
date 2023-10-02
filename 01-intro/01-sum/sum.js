659b2c0d0294322afad81ea9f112491ad94d6e7f
MaxB <max.bakalets@gmail.com>
2023-10-02 19:24:34 +0200

    add solution for 1-module 1-task 2-try

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,12 +1,8 @@
 function sum(a, b) {
-  try {
-    if (typeof(a||b) !== 'number') {
-      throw new TypeError;
-    }
-    return a + b;
-  } catch (err) {
-    return err.name;
+  if ([a, b].some((value) => typeof value !== 'number')) {
+    throw new TypeError();
   }
+  return a + b;
 }
-
+// косяки понял, учту их, спасибо
 module.exports = sum;
